85a10b122f62480fd425da7bad3041b780de864a
laopin <lao-pin@yandex.ru>
2016-01-08 13:26:23 +0200

    hid_keyboard
    hid_keyboard

---
+++ hid_keyboard/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'hid_keyboard' 
+ * Target:  'hid_keyboard Configuration' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ hid_mouse/MDK-ARM/hid_mouse Configuration/startup_stm32f072xb.d
@@ -0,0 +1,1 @@
+hid_mouse configuration\startup_stm32f072xb.o: ../Drivers/CMSIS/Device/ST/STM32F0xx/Source/Templates/arm/startup_stm32f072xb.s
